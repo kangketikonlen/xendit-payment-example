b1a34a00c7ce508091e7c42464ce62aa5d4abdd3
Gilang Pratama <pratamapriadi96@gmail.com>
2022-02-02 14:06:18 +0700

    :zap: Added ewallet callback

--- routers/callback_route.py
+++ routers/callback_route.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 from fastapi import Request
 from fastapi import APIRouter, HTTPException, exceptions
@@ -15,11 +16,18 @@ CB_TOKEN = os.environ["XENDIT_CB_TOKEN"]
 async def callback_virtual_account_created(reqs: Request):
     if reqs.headers.get('x-callback-token') == CB_TOKEN:
         callback_data = await reqs.body()
-        return callback_data
+        return json.loads(callback_data.decode('utf8').replace("'", '"'))
 
 
 @router.post("/virtual-account-paid")
 async def callback_virtual_account_paid(reqs: Request):
     if reqs.headers.get('x-callback-token') == CB_TOKEN:
         callback_data = await reqs.body()
-        return callback_data
+        return json.loads(callback_data.decode('utf8').replace("'", '"'))
+
+
+@router.post("/ewallet-payment-status")
+async def callback_ewallet_payment_status(reqs: Request):
+    if reqs.headers.get('x-callback-token') == CB_TOKEN:
+        callback_data = await reqs.body()
+        return json.loads(callback_data.decode('utf8').replace("'", '"'))
